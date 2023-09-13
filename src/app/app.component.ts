d70ebbcdd63ba027e294f6118da0831eff66f546
paskaliahahaha <paskalia.sarionsong11@gmail.com>
2023-09-13 11:21:27 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  appName = 'Metric Converter';
+  authorName = 'Paskalia R. Saroinsong';
+
+  selectedMetric: string = '';
+  selectedUnitFrom: string = '';
+  selectedUnitTo: string = '';
+  inputValue: number | null = null;
+  resultValue: number | null = null;
+
+  metrics = ['Panjang', 'Massa', 'Suhu'];
+  units: { [metric: string]: string[] } = {
+    Panjang: ['Meter', 'Centimeter', 'Kilometer'],
+    Massa: ['Gram', 'Kilogram', 'Milligram'],
+    Suhu: ['Celsius', 'Fahrenheit', 'Kelvin'],
+  };
+
+  onMetricChange(event: any) {
+    this.selectedMetric = event.detail.value;
+    this.selectedUnitFrom = '';
+    this.selectedUnitTo = '';
+    this.inputValue = null;
+    this.resultValue = null;
+  }
+
+  onUnitFromChange(event: any) {
+    this.selectedUnitFrom = event.detail.value;
+  }
+
+  onUnitToChange(event: any) {
+    this.selectedUnitTo = event.detail.value;
+  }
+
+  convert() {
+    if (isNaN(this.inputValue as number)) {
+      console.log('Input is not a valid number.');
+      return;
+    }
+
+    if (this.selectedMetric === 'Panjang') {
+      this.performPanjangConversion();
+    } else if (this.selectedMetric === 'Massa') {
+      this.performMassaConversion();
+    } else if (this.selectedMetric === 'Suhu') {
+      this.performSuhuConversion();
+    }
+  }
+
+  performPanjangConversion() {
+    const units: { [unit: string]: number } = {
+      Meter: 1,
+      Centimeter: 100,
+      Kilometer: 0.001,
+    };
+
+    const conversionFactor =
+      units[this.selectedUnitTo] / units[this.selectedUnitFrom];
+    this.resultValue = this.inputValue as number * conversionFactor;
+  }
+
+  performMassaConversion() {
+    const units: { [unit: string]: number } = {
+      Gram: 1,
+      Kilogram: 0.001,
+      Milligram: 1000,
+    };
+
+    const conversionFactor =
+      units[this.selectedUnitTo] / units[this.selectedUnitFrom];
+    this.resultValue = this.inputValue as number * conversionFactor;
+  }
+
+  performSuhuConversion() {
+    const units: { [unit: string]: number } = {
+      Celsius: 1,
+      Fahrenheit: 33.8,
+      Kelvin: 274.15,
+    };
+
+    const conversionFactor =
+      units[this.selectedUnitTo] / units[this.selectedUnitFrom];
+    this.resultValue = this.inputValue as number * conversionFactor;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), FormsModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
